af0c3ebd7aa17b27b319b87903516be79d6b0c28
Sang Mai <37669424+sangmai350@users.noreply.github.com>
2022-02-03 11:22:45 +0700

    Update base.api.js

--- apis/base.api.js
+++ apis/base.api.js
@@ -1,20 +1,9 @@
 const { client } = require('nightwatch-api');
 const env = require("../env/api.ENV");
 const { default: axios } = require('axios');
-const sql = require("mssql");
-// config for your database
-const config = {
-  user: 'sa',
-  password: 'Abcd123@',
-  server: 'localhost',
-  database: 'master',
-  options: {
-    trustedConnection: true,
-    encrypt: true,
-    enableArithAbort: true,
-    trustServerCertificate: true,
-  },
-};
+const sql = require('mssql/msnodesqlv8');
+
+
 class BaseAPI {
 
   constructor() {
@@ -26,10 +15,11 @@ class BaseAPI {
   }
 
   async connectToMSSql() {
-    sql.on('error', err => {
-      console.log(err);
-    })
-    return await sql.connect(config);
+    const config = {
+      driver: 'msnodesqlv8',
+      connectionString: 'Driver={SQL Server Native Client 11.0};Server={localhost};Database={MMABooks};Trusted_Connection={yes}',
+    };
+    return await new sql.connect(config);
   }
 
 }
